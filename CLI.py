3dc1f59c5e005e39058a953751287e916d50df28
KRZYSZTOF_KULKA <krzysztof.kulka1234@gmail.com>
2024-04-15 22:15:03 +0200

    continuing work on cli

--- CLI.py
+++ CLI.py
@@ -2,11 +2,55 @@ import argparse
 import subprocess
 
 def main():
-    parser = argparse.ArgumentParser(description='CLI')
-    parser.add_argument('script', type=str, help='Name of the script to run')
-    parser.add_argument('--arg', type=str, help='Argument for the script')
-    args = parser.parse_args()
-    subprocess.run(['python', f'{args.script}.py', args.arg])
+    parser = argparse.ArgumentParser(description="Utility CLI for various log processing scripts.")
+    subparsers = parser.add_subparsers(title="subcommands", dest="subcommand")
 
-if __name__ == '__main__':
+    # Subparser for get_ipv4s_from_log
+    ipv4s_parser = subparsers.add_parser("get_ipv4s_from_log", description="Extract IPv4s from log file.")
+    ipv4s_parser.add_argument("fileName", help="Name of the log file")
+    ipv4s_parser.add_argument("-l", "--log-level", default="INFO", help="Minimal logging level")
+
+    # Subparser for get_message_type
+    message_type_parser = subparsers.add_parser("get_message_type", description="Get message types from log file.")
+    message_type_parser.add_argument("fileName", help="Name of the log file")
+    message_type_parser.add_argument("-l", "--log-level", default="INFO", help="Minimal logging level")
+
+    # Subparser for log_statistic
+    statistic_parser = subparsers.add_parser("log_statistic", description="Get statistics from log file.")
+    statistic_parser.add_argument("fileName", help="Name of the log file")
+    statistic_parser.add_argument("-l", "--log-level", default="INFO", help="Minimal logging level")
+
+    # Subparser for print_dicts
+    print_dicts_parser = subparsers.add_parser("print_dicts", description="Print dictionaries from log file.")
+    print_dicts_parser.add_argument("fileName", help="Name of the log file")
+    print_dicts_parser.add_argument("-l", "--log-level", default="INFO", help="Minimal logging level")
+
+    # Subparser for random_logs_random_user
+    random_logs_parser = subparsers.add_parser("random_logs_random_user", description="Generate random logs for a user.")
+    random_logs_parser.add_argument("fileName", help="Name of the log file")
+    random_logs_parser.add_argument("-l","--log-level" default="INFO", help="Minimal logging level")
+
+    args = parser.parse_args()
+
+    if args.subcommand == "get_ipv4s_from_log":
+        run_script("get_ipv4s_from_log.py", args)
+    elif args.subcommand == "get_message_type":
+        run_script("get_message_type.py", args)
+    elif args.subcommand == "log_statistic":
+        run_script("log_statistic.py", args)
+    elif args.subcommand == "print_dicts":
+        run_script("print_dicts.py", args)
+    elif args.subcommand == "random_logs_random_user":
+        run_script("random_logs_random_user.py", args)
+    else:
+        parser.print_help()
+
+def run_script(script_name, args):
+    command = ["python", script_name, args.fileName]
+    if args.l:
+        command.extend([args.l])
+    
+    subprocess.run(command)
+
+if __name__ == "__main__":
     main()
--- get_ipv4s_from_log.py
+++ get_ipv4s_from_log.py
@@ -1,6 +1,9 @@
 import re
-
+import ssh_to_dict
+import sys
 def get_ipv4s_from_log(log_dict):
     ipv4_pattern = r"\b(?:\d{1,3}\.){3}\d{1,3}\b"
     ipv4_addresses = re.findall(ipv4_pattern, log_dict['description'])
     return ipv4_addresses
+if __name__ == "__main__":
+    get_ipv4s_from_log(ssh_to_dict.ssh_to_dict(sys.argv[1]))
